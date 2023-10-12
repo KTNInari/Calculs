49f749b30e1fd1e4de475d938d3c3b5b5401d101
Etude <Etude@DESKTOP-PEE1U7D>
2023-10-12 19:54:31 +0200

    Optimisation du code en n'écrivant qu'une foi la création des 2 nombres aléatoires.

--- Calculs/Program.cs
+++ Calculs/Program.cs
@@ -31,6 +31,10 @@ namespace Calculs
                 // traitement des choix
                 if (choix != 0)
                 {
+                    //création de nombres aléatoires
+                    val1 = rand.Next(1, 10);
+                    val2 = rand.Next(1, 10);
+
                     if (choix == 1)
                     {
                         // choix de l'addition
@@ -53,8 +57,6 @@ namespace Calculs
                     else
                     {
                         // choix de la multiplication
-                        val1 = rand.Next(1, 10);
-                        val2 = rand.Next(1, 10);
                         // saisie de la réponse
                         Console.Write(val1 + " x " + val2 + " = ");
                         reponse = int.Parse(Console.ReadLine());
